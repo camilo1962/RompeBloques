d4c6bd13eb5c68bd6c3a40e246d38e6d6f2eaecb
camilo1962 <108455047+camilo1962@users.noreply.github.com>
2024-03-26 12:47:41 +0100

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void BorraRecord()
+    {
+        PlayerPrefs.DeleteKey("record");
+    }
+}
